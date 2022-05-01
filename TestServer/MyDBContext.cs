ac9a6000c2d5985df1e6edcd2a261251805a6a67
Viktoria Sadivska <vika.sadivska@gmail.com>
2022-05-01 03:16:50 +0300

    added DB context and intializer

---
+++ TestServer/MyDBContext.cs
@@ -0,0 +1,24 @@
+﻿using DBLib;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace TestServer
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext() : base("conStr") { }
+        static MyDBContext()    
+        {
+            Database.SetInitializer<MyDBContext>(new MyInitializeDB());
+        }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Group> Groups { get; set; }
+        public virtual DbSet<Test> Tests { get; set; }
+        public virtual DbSet<Question> Questions { get; set; }
+        public virtual DbSet<Answer> Answers { get; set; }
+        public virtual DbSet<AssignedTest> AssignedTests { get; set; }
+        public virtual DbSet<UserAnswer> UserAnswers { get; set; }
+    }
+}
---
+++ TestServer/MyInitializeDB.cs
@@ -0,0 +1,19 @@
+﻿using DBLib;
+using System.Data.Entity;
+
+namespace TestServer
+{
+    public class MyInitializeDB : CreateDatabaseIfNotExists<MyDBContext>
+    {
+        protected override void Seed(MyDBContext context)
+        {
+            User user = new User { FirstName = "admin", LastName = "admin", Login = "admin", Password = "admin" };
+            Group group = new Group { Name = "admins" };
+            group.Users.Add(user);
+            context.Users.Add(user);
+            context.Groups.Add(group);
+   
+            context.SaveChanges();
+        }
+    }
+}
