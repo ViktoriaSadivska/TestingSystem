37a5b39e34423009895f85354aaddb8427375d15
Viktoria Sadivska <vika.sadivska@gmail.com>
2022-05-03 16:15:50 +0300

    added TestingWindow design

---
+++ TestClient/TestingWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestClient
+{
+    /// <summary>
+    /// Interaction logic for TestingWindow.xaml
+    /// </summary>
+    public partial class TestingWindow : Window
+    {
+        public TestingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PrevQstnButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NextQstnButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FinishButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
